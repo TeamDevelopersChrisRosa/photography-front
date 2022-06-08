d48d0ecc2e7eff0f73b4de4e16b4aa2dd95e2dc7
hyyspeed <christopher@lamirau.com>
2022-06-08 16:32:07 +0200

    wip component

---
+++ src/components/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Home = () => {
+
+  return (
+    <>
+        <p>Home</p>
+    </>
+  );
+};
+
+Home.propTypes = {
+};
+
+Home.defaultProps = {
+};
+
+export default Home;
