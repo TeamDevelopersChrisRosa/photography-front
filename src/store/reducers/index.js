282fcd6266e26f150ef8ba8a5f07cb0e21ba3b18
hyyspeed <christopher@lamirau.com>
2022-06-08 16:22:34 +0200

    architecture
    Co-authored-by: RosaHuisman <RosaHuisman@users.noreply.github.com>

---
+++ src/store/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+// import reducers here: sample:
+// import authReducer from './authentification';
+
+
+const rootReducer = combineReducers({
+  // all reducers here
+
+});
+
+export default rootReducer;
