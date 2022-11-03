3783f6220c43a7f55123367a171d1362e34b3649
RosaHuisman <rosa.huisman@gmail.com>
2022-11-03 14:10:14 +0100

    token in the headers

--- src/store/middlewares/utils/api.js
+++ src/store/middlewares/utils/api.js
@@ -4,6 +4,11 @@ const api = axios.create({
   //baseURL: 'http://localhost:3000/api',
   baseURL: process.env.REACT_APP_API_URL,
   timeout: 2500,
+  headers: {
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
 });
 
 export default api;
+
+
