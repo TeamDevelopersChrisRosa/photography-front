80bcf8d9edee4067bbbccc434c2d86b0ec9f2036
Rosa Huisman <rosa.huisman@gmail.com>
2022-11-29 19:15:28 +0100

    upload & delete picture ameliorations

--- src/store/middlewares/picture.js
+++ src/store/middlewares/picture.js
@@ -11,29 +11,17 @@ import api from './utils/api';
 const picturemiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case DELETE_PICTURE: {
-    // first we delete the picture on cloudinary and then we delete it on the database
       api({
-        method: 'POST',
-        url: 'picture/destroy',
+        method: 'DELETE',
+        url: `picture/${action.pictureId}`,
         data: {
           publicId: action.publicId,
         },
       })
         .then((response) => {
-          if (response.data.result === 'ok') {
-            api({
-              method: 'DELETE',
-              url: `picture/${action.pictureId}`,
-
-            })
-              .then((response) => {
-                store.dispatch(refreshShooting(action.pictureId, action.shootingId));
-              })
-              .catch((error) => {
-                  console.log(error)
-                });
-            }
-          })
+          console.log(response);
+          store.dispatch(refreshShooting(action.pictureId, action.shootingId));
+        })
         .catch((error) => {
             console.log(error)
           });
@@ -41,32 +29,14 @@ const picturemiddleware = (store) => (next) => (action) => {
     }
 
     case UPLOAD_IMAGE: {
-      // first we upload the image to cloudinary and then we save the picture in the database
+      // we send the picture to the server in body, and with the shootingId and the share boolean in parameters
       api({
         method: 'POST',
-        url: 'picture/upload',
-        data: action.formData, 
+        url: `picture/upload/${action.share}/${action.shootingId}`,
+        data: action.formData,        
       })
         .then((response) => {
-          api({
-            method: 'POST',
-            url: 'picture',
-            data: {
-              shootingId: action.shootingId,
-              share: action.share,
-              path: response.data.public_id,
-              name: action.formData.get('file').name,
-              secureUrl: response.data.secure_url,
-              width: response.data.width,
-              height: response.data.height,
-            },
-          })
-            .then((response) => {
-              store.dispatch(addPictureInShootingOnState(response.data, action.shootingId));
-            })
-            .catch((error) => {
-                console.log(error)
-              });
+          store.dispatch(addPictureInShootingOnState(response.data, action.shootingId));
         })
         .catch((error) => {
             console.log(error)
